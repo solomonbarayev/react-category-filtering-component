0cd7f67f4fb047198d8f6ee62f7b41205b3441c2
solomonbarayev <sbarayev2@gmail.com>
2022-07-19 09:49:26 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import Menu from "./Menu";
+import Categories from "./Categories";
+import items from "./data";
+
+const allCategories = ["all", ...new Set(items.map((item) => item.category))];
+
+console.log(allCategories);
+
+function App() {
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState(allCategories);
+
+  console.log(categories);
+
+  const filterItems = (category) => {
+    if (category === "all") {
+      return setMenuItems(items);
+    }
+    const newItems = items.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  };
+
+  return (
+    <main>
+      <section className="menu section">
+        <div>
+          <h2 className="title">menu project setup</h2>
+          <div className="underline"></div>
+        </div>
+      </section>
+      <Categories filterItems={filterItems} categories={categories} />
+      <Menu items={menuItems} />
+    </main>
+  );
+}
+
+export default App;
