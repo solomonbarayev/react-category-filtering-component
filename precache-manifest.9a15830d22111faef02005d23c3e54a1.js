ea8a9df8ad4dcafb81da199665ce610536f6853f
solomonbarayev <sbarayev2@gmail.com>
2022-07-19 09:53:25 +0300

    Updates

---
+++ precache-manifest.9a15830d22111faef02005d23c3e54a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f3899102be4d3dba7f23150df3b2bb0",
+    "url": "/react-category-filtering-component/index.html"
+  },
+  {
+    "revision": "ba61dbc6a47c13a60dc9",
+    "url": "/react-category-filtering-component/static/css/main.99af19dc.chunk.css"
+  },
+  {
+    "revision": "6e331cfd5d1a1283573a",
+    "url": "/react-category-filtering-component/static/js/2.66b87b03.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-category-filtering-component/static/js/2.66b87b03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba61dbc6a47c13a60dc9",
+    "url": "/react-category-filtering-component/static/js/main.5f445999.chunk.js"
+  },
+  {
+    "revision": "4e359f2c52a8f03a4b0d",
+    "url": "/react-category-filtering-component/static/js/runtime-main.fdae1521.js"
+  }
+]);
